a3c54548c62ec5386fc8bb5c46948db0126abd68
Felix Mattick <felix.mattick@gmail.com>
2019-02-18 13:32:35 -0500

    Add meat of api

---
+++ api/getters/deployment.js
@@ -0,0 +1,33 @@
+require('../mongoose')
+const Deployment = require('../schemas/Deployment')
+const { memes } = require('../constants')
+const { parse } = require('url')
+
+module.exports = async (req, res) => {
+  try {
+    const { code } = parse(req.url, true).query
+    if (!code) {
+      res.statusCode = 400
+      res.setHeader('Content-Type', 'text/plain')
+      return res.end('No deployment specified')
+    }
+
+    const deployment = await Deployment.findOne({ code })
+    if (!deployment) {
+      res.statusCode = 404
+      res.setHeader('Content-Type', 'text/plain')
+      return res.end('Deployment not found')
+    }
+
+    deployment.views++
+    await deployment.save()
+
+    res.statusCode = 301
+    res.setHeader('Location', memes[deployment.memeIndex])
+    res.end()
+  } catch(error) {
+    res.statusCode = 500
+    res.setHeader('Content-Type', 'text/plain')
+    return res.end('We messed up')
+  }
+}
--- api/routes.js
+++ api/routes.js
@@ -6,7 +6,7 @@ const User = require('./schemas/User')
 const Deployment = require('./schemas/Deployment')
 
 module.exports = async (app) => {
-  app.post('/api/key', async (req, res) => {
+  app.post('/api/key/get', async (req, res) => {
     try {
       const { username, password } = req.body
       if (!username || !password) return res.status(401).json({
@@ -36,7 +36,7 @@ module.exports = async (app) => {
     }
   })
   
-  app.post('/api/regen', async (req, res) => {
+  app.post('/api/key/regen', async (req, res) => {
     try {
       const { username, password } = req.body
       if (!username || !password) return res.status(400).json({
@@ -66,7 +66,7 @@ module.exports = async (app) => {
     }
   })
 
-  app.post('/api/deploy', async (req, res) => {
+  app.post('/api/deployments/create', async (req, res) => {
     try {
       const { key, memeIndex } = req.body
       if (!key) return res.status(401).json({
@@ -93,6 +93,143 @@ module.exports = async (app) => {
       res.json({
         code,
         uri: `https://raas.pw/r/${code}`
+      })
+    } catch(error) {
+      res.status(500).json({
+        error: 'We messed up'
+      })
+    }
+  })
+
+  app.post('/api/deployments/info', async (req, res) => {
+    try {
+      const { key, code } = req.body
+      if (!key) return res.status(401).json({
+        error: 'API key not specified'
+      })
+      if (!code) return res.status(400).json({
+        error: 'Code not specified'
+      })
+
+      const existingUser = await User.findOne({ key })
+      if (!existingUser) return res.status(401).json({
+        error: 'Invalid API key'
+      })
+
+      const deployment = await Deployment.findOne({ code })
+      if (!deployment) return res.status(404).json({
+        error: 'Deployment not found'
+      })
+      if (deployment.username !== existingUser.username) {
+        return res.status(403).json({
+          error: 'You are not the owner of that deployment'
+        })
+      }
+
+      res.json({
+        memeIndex: deployment.memeIndex,
+        memeUri: memes[deployment.memeIndex],
+        views: deployment.views,
+        uri: deployment.uri
+      })
+    } catch(error) {
+      res.status(500).json({
+        error: 'We messed up'
+      })
+    }
+  })
+
+  app.post('/api/deployments/list', async (req, res) => {
+    try {
+      const { key } = req.body
+      if (!key) return res.status(401).json({
+        error: 'API key not specified'
+      })
+
+      const existingUser = await User.findOne({ key })
+      if (!existingUser) return res.status(401).json({
+        error: 'Invalid API key'
+      })
+
+      const deployments = await Deployment.find({
+        username: existingUser.username
+      }, 'code memeIndex views -_id')
+      res.json({
+        count: deployments.length,
+        deployments
+      })
+    } catch(error) {
+      res.status(500).json({
+        error: 'We messed up'
+      })
+    }
+  })
+
+  app.post('/api/deployments/view', async (req, res) => {
+    try {
+      const { key, code } = req.body
+      if (!key) return res.status(401).json({
+        error: 'API key not specified'
+      })
+      if (!code) return res.status(400).json({
+        error: 'Code not specified'
+      })
+
+      const existingUser = await User.findOne({ key })
+      if (!existingUser) return res.status(401).json({
+        error: 'Invalid API key'
+      })
+
+      const deployment = await Deployment.findOne({ code })
+      if (!deployment) return res.status(404).json({
+        error: 'Deployment not found'
+      })
+      if (deployment.username !== existingUser.username) {
+        return res.status(403).json({
+          error: 'You are not the owner of that deployment'
+        })
+      }
+
+      deployment.views++
+      await deployment.save()
+      res.json({
+        views: deployment.views
+      })
+    } catch(error) {
+      res.status(500).json({
+        error: 'We messed up'
+      })
+    }
+  })
+
+  app.delete('/api/deployments/delete', async (req, res) => {
+    try {
+      const { key, code } = req.body
+      if (!key) return res.status(401).json({
+        error: 'API key not specified'
+      })
+      if (!code) return res.status(400).json({
+        error: 'Code not specified'
+      })
+
+      const existingUser = await User.findOne({ key })
+      if (!existingUser) return res.status(401).json({
+        error: 'Invalid API key'
+      })
+
+      const deployment = await Deployment.findOne({ code })
+      if (!deployment) return res.status(404).json({
+        error: 'Deployment not found'
+      })
+      if (deployment.username !== existingUser.username) {
+        return res.status(403).json({
+          error: 'You are not the owner of that deployment'
+        })
+      }
+
+      await deployment.remove()
+      res.json({
+        finalViews: deployment.views
       })
     } catch(error) {
       res.status(500).json({
