fbf90d6b6f2fc7c6d587befdb5dd21e83f9df847
Felix Mattick <felix.mattick@gmail.com>
2019-02-18 21:11:43 -0500

    Add alias getter

---
+++ api/getters/alias.js
@@ -0,0 +1,42 @@
+require('../mongoose')
+const Alias = require('../schemas/Alias')
+const Deployment = require('../schemas/Deployment')
+const { memes } = require('../constants')
+const { parse } = require('url')
+
+module.exports = async (req, res) => {
+  try {
+    const { alias } = parse(req.url, true).query
+    if (!alias) {
+      res.statusCode = 400
+      res.setHeader('Content-Type', 'text/plain')
+      return res.end('No alias specified')
+    }
+
+    const alias = await Alias.findOne({ alias })
+    if (!alias) {
+      res.statusCode = 404
+      res.setHeader('Content-Type', 'text/plain')
+      return res.end('Alias not found')
+    }
+    const { code } = alias
+
+    const deployment = await Deployment.findOne({ code })
+    if (!deployment) {
+      res.statusCode = 404
+      res.setHeader('Content-Type', 'text/plain')
+      return res.end('Deployment not found')
+    }
+
+    deployment.views++
+    await deployment.save()
+
+    res.statusCode = 301
+    res.setHeader('Location', memes[deployment.memeIndex].uri)
+    res.end()
+  } catch(error) {
+    res.statusCode = 500
+    res.setHeader('Content-Type', 'text/plain')
+    return res.end('We messed up')
+  }
+}
