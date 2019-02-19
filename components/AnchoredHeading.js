6035fecb23e69b1573ba4b3671cb94b8c115699d
Felix Mattick <felix.mattick@gmail.com>
2019-02-18 22:15:09 -0500

    FIx regex

--- components/AnchoredHeading.js
+++ components/AnchoredHeading.js
@@ -1,5 +1,5 @@
 export default ({ children }) => {
-  const id = children.toLowerCase().replace(/\s+/g, '-')
+  const id = children.toLowerCase().replace(/\W+/g, '-')
   return (
     <h2 className='mb-4' id={id}>
       {children + ' '}
